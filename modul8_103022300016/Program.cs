a2cefe61f034a9f716cd4e9dcc280b27403f4937
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 17:04:14 +0700

    Aplikasi bank transfer dengan runtime configuration

--- modul8_103022300016/Program.cs
+++ modul8_103022300016/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace modul8_103022300016
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            BankConfig bankConfig = new BankConfig();
+            
+            Console.WriteLine("Aplikasi Bank Holo");
+            Console.WriteLine("====================================");
+            Console.WriteLine("Type a language (id/en)");
+            string pilihanBahasa = Console.ReadLine();
+            if (pilihanBahasa == "id")
+            {
+                bankConfig.ubahBahasa();
+                Console.Write("Masukkan jumlah uang yang akan di transfer: ");
+                int uang = Convert.ToInt32(Console.ReadLine());
+
+                if (uang < bankConfig.config.transfer.threshold)
+                {
+                    Console.WriteLine($"Biaya transfer: {bankConfig.config.transfer.low_fee} dan Total Biaya = {uang + bankConfig.config.transfer.low_fee}");
+                }
+                else
+                {
+                    Console.WriteLine($"Biaya transfer: {bankConfig.config.transfer.high_fee} dan Total Biaya = {uang + bankConfig.config.transfer.high_fee}");
+                }
+
+                Console.WriteLine("Pilih metode transfer: ");
+                for (int i = 0; i < bankConfig.config.methods.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + bankConfig.config.methods[i]);
+                }
+                Console.Write("Pilih: ");
+                int metodeTransaksi = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Ketik {bankConfig.config.confirmation.id} untuk mengkonfirmasi transaksi");
+                string konfirmasi = Console.ReadLine();
+
+                if (konfirmasi != bankConfig.config.confirmation.id)
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                    return;
+                }
+
+                Console.WriteLine("Proses Tranasaksi Berhasil");
+            }
+            else if (pilihanBahasa == (bankConfig.config.lang))
+            {
+                bankConfig.ubahBahasa();
+                Console.Write("Please insert the amount of money to transfer: ");
+                int uang = Convert.ToInt32(Console.ReadLine());
+
+                if (uang < bankConfig.config.transfer.threshold)
+                {
+                    Console.WriteLine($"Transfer fee: {bankConfig.config.transfer.low_fee} and Total Amount = {uang + bankConfig.config.transfer.low_fee}");
+                }
+                else
+                {
+                    Console.WriteLine($"Transfer fee: {bankConfig.config.transfer.high_fee} and Total Amount = {uang + bankConfig.config.transfer.high_fee}");
+                }
+                Console.WriteLine("Select transfer method: ");
+                for (int i = 0; i < bankConfig.config.methods.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + bankConfig.config.methods[i]);
+                }
+
+                Console.Write("Choose: ");
+                int metodeTransaksi = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Type {bankConfig.config.confirmation.en} to confirm transaction");
+                string konfirmasi = Console.ReadLine();
+
+                if (konfirmasi != bankConfig.config.confirmation.en)
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                    return;
+                }
+
+                Console.WriteLine("The Transfer is Completed");
+            }
+        }
+    }
+}
---
+++ modul8_103022300016/bankConfig.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300016
+{
+    class Transfer
+    {
+        public int threshold { get; set; } // getter setter untuk threshold
+        public int low_fee { get; set; } // getter setter low_fee
+        public int high_fee { get; set; } // getter setter high_fee
+    }
+
+    class Confirmation
+    {
+        public string id { get; set; } // getter setter id
+        public string en { get; set; } // getter setter en
+    }
+
+    class Config
+    {
+        // attribute untuk deserialisasi
+        public string lang { get; set; } // getter setter lang
+        public Transfer transfer { get; set; } // getter setter transfer
+        public List<string> methods { get; set; } // getter setter method
+        public Confirmation confirmation { get; set; } // getter setter confirmation
+
+        // constructor kosong untuk deserialisasi
+        public Config()
+        {
+        }
+
+        // constructor untuk config
+        public Config(string lang, Transfer transfer, List<string> methods, Confirmation confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+
+    }
+    class BankConfig
+    {
+
+        public Config config;
+        // filePath Json
+        public String filePath = @"bank_transfer_config.json";
+
+        public BankConfig()
+        {
+            try
+            // exception untuk mengatasi error
+            {
+                config = ReadConfigFile(); // memanggil method readConfigFile
+            }
+            catch (Exception ex)
+            {
+                setDefault(); // memanggil method setDefault
+                WriteConfigFile(); // memanggil method WriteConfigFile
+            }
+        }
+
+        private Config ReadConfigFile()
+        {
+            string Filejson = File.ReadAllText(filePath); // Membaca file json
+            Config config = JsonSerializer.Deserialize<Config>(Filejson); // melakukan deserialisasi file json
+            return config;
+        }
+
+        private void WriteConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true 
+            };
+
+            string jsonString = JsonSerializer.Serialize(config, options); // serialisasi json
+            File.WriteAllText(filePath, jsonString); // menulis file json
+        }
+
+        public void ubahBahasa()
+        // method untuk mengubah bahasa
+        {
+            if (config.lang == "en")
+            {
+                config.lang = "id";
+            }
+            else
+            {
+                config.lang = "en";
+            }
+        }
+
+        private void setDefault()
+        // method untuk set default
+        {
+            config = new Config();
+            config.lang = "en";
+            config.transfer = new Transfer();
+            config.transfer.threshold = 25000000;
+            config.transfer.low_fee = 6500;
+            config.transfer.high_fee = 15000;
+            config.methods = new List<string>();
+            config.methods.Add("RTO (Real Time");
+            config.methods.Add("SKN");
+            config.methods.Add("RTGS");
+            config.methods.Add("BI Fast");
+            config.confirmation = new Confirmation();
+            config.confirmation.id = "ya";
+            config.confirmation.en = "yes";
+        }
+
+
+    }
+}
